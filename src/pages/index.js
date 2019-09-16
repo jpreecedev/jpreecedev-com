38eb3341717ef057898ad89598c8453e77330f15
Jon Preece <jonpreece@hotmail.co.uk>
2019-09-16 19:04:33 +0100

    release v3.0.4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import * as React from "react"
 
 import jonpreece from "../images/jonpreece-square.jpg"
-import javascript from "../images/javascript.png"
-import react from "../images/react.png"
 
 import "../styles.css"
 
@@ -13,10 +11,6 @@ const IndexPage = () => (
       <h1>Jon Preece</h1>
       <h2>Specialist Front-end Web Developer</h2>
     </header>
-    <section id="skills">
-      <img src={javascript} alt="JavaScript" />
-      <img src={react} alt="React" />
-    </section>
     <section id="contact" className="contact">
       <p>
         Connect with me on <a href="https://www.linkedin.com/in/jonpreecedev/">LinkedIn</a>
