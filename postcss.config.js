7a57f5ce4e29224463bc032c101d1d0c0752f497
Jon Preece <>
2023-02-04 17:08:49 +0000

    migrate to astro

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
-}
+  plugins: [
+    require("postcss-nesting"),
+    require("postcss-preset-env")({ stage: 0 }),
+  ],
+};
